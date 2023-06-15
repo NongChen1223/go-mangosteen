56ef2b284357543027630b50c3da901f4a3be108
NongChen1223 <2584533077@qq.com>
2023-06-15 23:08:33 +0800

    update:连接本地数据库

--- internal/database/mysql.go
+++ internal/database/mysql.go
@@ -8,15 +8,15 @@ import (
 )
 
 const (
-	mysqlHost     = "172.18.0.2"
-	mysqlPort     = 3306
+	mysqlHost     = "localhost"
+	mysqlPort     = 3307
 	mysqlUser     = "mangosteen"
 	mysqlPassword = "123456"
 	mysqlDbname   = "mangosteen_dev"
 )
 
 func MysqlConnect() {
-	connStr := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", mysqlUser, mysqlPassword, mysqlHost, mysqlDbname)
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", mysqlUser, mysqlPassword, mysqlHost, mysqlPort, mysqlDbname)
 	db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalln("sql链接失败 打印", err)
@@ -28,6 +28,22 @@ func MysqlConnect() {
 	}
 	log.Println("sql链接成功")
 }
+func MysqlCreateTable() {
+	createTableSQL := `CREATE TABLE IF NOT EXISTS users(
+    	id INT AUTO_INCREMENT PRIMARY KEY,
+    	email VARCHAR(255) NOT NULL,
+    	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );`
+	_, err := DB.Exec(createTableSQL)
+
+	if err != nil {
+		log.Fatalln("创建表失败 打印:", err)
+	}
+
+	log.Println("创建表成功")
+}
+
 func MysqlClose() {
 	DB.Close()
 	log.Println("sql链接关闭")
